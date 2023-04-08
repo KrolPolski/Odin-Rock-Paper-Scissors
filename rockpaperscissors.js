21b5145427db446141011f1d3280093fd2419d05
KrolPolski <ryanboudwin@outlook.com>
2023-04-08 21:41:16 +0300

    single round resolution implemented

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,13 +1,15 @@
 // define choices
 const choices = ["rock", "paper", "scissors"];
-
+let computerChoice;
+let playerChoice;
 console.log("Welcome to Rock Paper Scissors!")
 // Get the choice from the computer
 
 function getComputerChoice()
 {
     let randomNumber = Math.floor(Math.random() * 3);
-    let computerChoice = choices[randomNumber];
+    computerChoice = choices[randomNumber];
+    return computerChoice;
     // console.log(computerChoice);
    // console.log(randomNumber);
    // console.log(choices[randomNumber]);
@@ -32,6 +34,64 @@ function getPlayerChoice()
 
 getPlayerChoice();
 // resolve who wins
+
+function resolveRound(playerChoice, computerChoice)
+{
+    let roundOutcome;
+
+    if (playerChoice === computerChoice)
+    {
+        roundOutcome === "tie";
+        console.log("You both chose " + playerChoice + ", so it is a tie.");
+        return roundOutcome;
+    }
+    else switch (playerChoice) 
+    {
+        case 'rock':
+            switch (computerChoice) 
+                {
+                    case 'scissors':
+                        console.log("Rock crushes scissors, so you win!");
+                        roundOutcome = "victory";
+                        break;
+                        
+                    case 'paper':
+                        console.log("Paper covers rock, so you lose!");
+                        roundOutcome = "defeat";
+                        break;
+                }
+            break;
+        case 'scissors':
+            switch (computerChoice)
+                {
+                    case 'rock':
+                        console.log("Rock crushes scissors, you lose!");
+                        roundOutcome = "defeat";
+                        break;
+                    case 'paper':
+                        console.log("Scissors cuts paper, you win!");
+                        roundOutcome = "victory";
+                        break;
+                       
+                }
+            break;     
+        case 'paper':
+            switch (computerChoice)
+                {
+                    case 'rock':
+                        console.log("Paper covers rock, you win!");
+                        roundOutcome = "victory";
+                        break;
+                    case 'scissors':
+                        console.log("Scissors cuts paper, you lose!");
+                        roundOutcome = "defeat";
+                        break;
+                }
+            break;
+    }   
+}
+
+resolveRound(playerChoice, computerChoice)
 // report who wins to the user
 // update score so far
 // determine if there should be a new round or if the game has ended
